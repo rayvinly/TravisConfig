5948557adbce51d76ec693d1100c5d0c5ea8a05e
Raymond Law <ray@clean-swift.com>
2017-07-07 03:09:42 -0400

    New project

---
+++ TravisConfig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisConfig
+//
+//  Created by Raymond Law on 7/7/17.
+//  Copyright © 2017 Clean Swift LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
